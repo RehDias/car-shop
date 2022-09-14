24806edebe922b9bfcf4a650274e5a436981ffdd
renata dias <renata_dias96@live.com>
2022-09-14 17:39:26 -0300

    feat: AbstModel
    Cria um model abstrato que implementa o IModel e seus métodos.

---
+++ src/models/AbstModel.ts
@@ -0,0 +1,31 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+export default abstract class AbstModel<T> implements IModel<T> {
+  constructor(protected _model: Model<T>) {}
+
+  public async create(obj: T): Promise<T> {
+    const result = await this._model.create(obj);
+    return result;
+  }
+
+  public async read(): Promise<T[]> {
+    const result = await this._model.find();
+    return result;
+  }
+
+  public async readOne(id: string): Promise<T | null> {
+    const result = await this._model.findOne({ id });
+    return result;
+  }
+
+  public async update(id: string, obj: T): Promise<T | null> {
+    const result = await this._model.updateOne({ id }, { obj });
+    return result as T;
+  }
+
+  public async delete(id: string): Promise<T | null> {
+    const result = await this._model.deleteOne({ id });
+    return result as T;
+  }
+}
