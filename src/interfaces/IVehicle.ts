528e7aead540a63b692ce34044307c2238eef340
renata dias <renata_dias96@live.com>
2022-09-14 16:57:09 -0300

    refactor: Interface IVehicle
    Refatora para fazer as validações dos atributos com o zod.

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,12 @@
-export interface IVehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number
-}
+import { z } from 'zod';
+
+const VehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().positive().gte(1900)
+    .lte(2022),
+  color: z.string().min(3),
+  status: z.optional(z.boolean()),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof VehicleZodSchema>;
