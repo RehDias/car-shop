04203800ff29b1ebcaf4caad6a5df47565166b83
renata dias <renata_dias96@live.com>
2022-09-15 15:13:41 -0300

    test: carController
    cria testes para o método create do car controller.

---
+++ src/tests/unit/controllers/car.test.ts
@@ -0,0 +1,38 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { Request, Response } from 'express';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import CarController from '../../../controllers/CarController';
+import { carMock, carMockResponse } from '../../mocks/carMock';
+const { expect } = chai;
+
+describe('car controller', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(async () => {
+    sinon.stub(carService, 'create').resolves(carMockResponse);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  });
+
+  describe('create', () => {
+    it('deve retornar status 201 e o carro criado', async () => {
+      req.body = carMock;
+      await carController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMockResponse)).to.be.true;
+    })
+  });
+});
